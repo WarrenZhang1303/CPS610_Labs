78bbfe63e9b30db21438e0c9c414015b542d29f1
PeirongZhang_MBP <warrenzhang1303@gmail.com>
2025-01-23 10:20:21 -0500

    renamed

---
+++ Lab01/EMPLOYEE.sql
@@ -0,0 +1,31 @@
+SELECT INSTANCE_NAME, HOST_NAME, VERSION FROM V$INSTANCE;
+
+SELECT SYS_CONTEXT('USERENV', 'SERVICE_NAME') AS SERVICE_NAME FROM DUAL;
+
+SELECT SYS_CONTEXT('USERENV', 'INSTANCE_NAME') AS INSTANCE_NAME,
+       SYS_CONTEXT('USERENV', 'HOST') AS HOST
+FROM DUAL;
+
+DROP DATABASE LINK db2;
+CREATE DATABASE LINK db2
+CONNECT TO p7zhang IDENTIFIED BY "03178102"
+USING
+'(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle12c.scs.ryerson.ca)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=orcl12c)))';
+
+SELECT * FROM p7zhang.DB2_TEST@db2;
+INSERT INTO p7test.tests@db2 (test_id, test_name) VALUES
+(1,'Hello! Can you see me!');
+//GRANT CONNECT, RESOURCE TO p7test;
+
+CREATE TABLE p7zhang.tests(
+    test_id number PRIMARY KEY,
+    test_name VARCHAR2(50)
+);
+INSERT INTO p7zhang.DB2_test@db2 (test_id, test_name) VALUES
+(2,'Hello! Can you see me! Message from DB1');
+
+ SELECT * FROM db2_test@db2;
+ 
+ SELECT *
+FROM db2_test@db2
+WHERE owner = 'p7zhang';
---
+++ Lab01/p7test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE db2_test(
+test_id number PRIMARY KEY,
+test_name VARCHAR2(50)
+);
+INSERT INTO db2_test(test_id, test_name) VALUES 
+(1,'YOU ARE IN THE DB2 RIGHT NOW');
